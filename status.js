755c62352707e2fb740e135f6b31b5d4829bd1fd
Glitch (glitch-hello-website) <none>
2022-05-01 18:03:41 +0000

    ✊💍 Checkpoint ./ViewerError.js:42601775/1174 ./status.js:42601775/1167 ./service-worker.js:42601775/1272

---
+++ status.js
@@ -0,0 +1,15 @@
+// vou arrumar
+
+"use strict";
+
+export default class Status {
+  #nome;
+  static NAVEGANDO = new Status('Navegando');
+  static INCLUINDO = new Status('Incluindo');  
+  static ALTERANDO = new Status('Alterando');  
+  static EXCLUINDO = new Status('Excluindo');  
+  
+  constructor(nome) {
+    this.#nome = nome;
+  }  
+}
